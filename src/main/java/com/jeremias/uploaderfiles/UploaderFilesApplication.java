5336861148955512c47b025d79293b740d4dbd59
Jeremias Adriano <109630558+jeremiasadriano@users.noreply.github.com>
2024-08-25 18:57:57 +0200

    init project

---
+++ src/main/java/com/jeremias/uploaderfiles/UploaderFilesApplication.java
@@ -0,0 +1,13 @@
+package com.jeremias.uploaderfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UploaderFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UploaderFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jeremias/uploaderfiles/UploaderFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeremias.uploaderfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UploaderFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
